00314c4222231aa50da6e74732602cd9e27d31f7
Vladyslav Protasenia <vprotasenia@devforth.io>
2024-04-11 17:41:30 +0300

    implement stages for service build\deploy

--- service/deploy/deploy.sh
+++ service/deploy/deploy.sh
@@ -4,16 +4,27 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+export STAGE=${STAGE:=latest}
+
+if [[ "$STAGE" == "stable" ]]; then
+  export OUTPORT=3000
+elif [[ "$STAGE" == "latest" ]]; then
+  export OUTPORT=3001
+else
+  echo "STAGE can only be 'stable' or 'latest'"
+  exit 1
+fi
+
 USERNAME=${USERNAME:-vprotasenia}
 MANAGER_HOSTNAME=34.140.119.26
 
 function deploy() {
   cd "${SCRIPT_DIR}"
 
-  docker build .. -t europe-central2-docker.pkg.dev/unitydiffusion/sd-experiments/service
-  docker push europe-central2-docker.pkg.dev/unitydiffusion/sd-experiments/service
+  docker build .. -t europe-central2-docker.pkg.dev/unitydiffusion/sd-experiments/service:${STAGE}
+  docker push europe-central2-docker.pkg.dev/unitydiffusion/sd-experiments/service:${STAGE}
 
-  docker -H ssh://${USERNAME}@${MANAGER_HOSTNAME} stack deploy -c docker-compose.yaml --detach=true --resolve-image always --with-registry-auth --prune "sd-experiments"
+  docker -H ssh://${USERNAME}@${MANAGER_HOSTNAME} stack deploy -c docker-compose.yaml --detach=true --resolve-image always --with-registry-auth --prune "sd-experiments-${STAGE}"
 }
 
 deploy
