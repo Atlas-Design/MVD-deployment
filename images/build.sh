d8d11f505397601e42d0488cf55b0ac3f3c0a3af
Vladyslav Protasenia <vprotasenia@devforth.io>
2024-04-11 17:14:15 +0300

    fix default path to sd_experiments folder

--- images/build.sh
+++ images/build.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 function build() {
   cd "${SCRIPT_DIR}"
 
-  export SD_EXPERIMENTS_HOME=${SD_EXPERIMENTS_HOME:=../../MVD-deployment}
+  export SD_EXPERIMENTS_HOME=${SD_EXPERIMENTS_HOME:=../../sd_experiments}
 
   docker buildx bake -f blender.docker-bake.hcl --push
   docker buildx bake -f comfywr.docker-bake.hcl --push
